9446f1182fda757867a9126ea7bffcae69e306af
lyndon bowen <lyndon bowen@DellWin8>
2017-08-19 17:26:19 -0400

    Push2! Lyndon

--- Pages/home.php
+++ Pages/home.php
@@ -1,165 +1,315 @@
-
 <?php
 
 include "../Functions/links.php";
 include "../Functions/query.php";
 
-
-$U_id = $_SESSION['U_id'];
-$get = fetch(query("select * from users where U_id = '$U_id'"));
-$_SESSION['Job_Title'] = $get['Job_Title']; 
-$_SESSION['Company_N'] = $get['Company_N'];
-$_SESSION['Company_E'] = $get['Company_E']; 
-$_SESSION['Profile_Descrip'] = $get['Profile_Descrip'];
-
 ?>
+	<!-- top two elements -->
+	<div class=" container col-lg-12 col-md-12 col-sm-12" style="height:350px; background-color:#d01d3a; padding-left: 0px; padding-right: 0px;">
+
+		<div id="content1" class="container panel panel-inverse col-lg-4 col-lg-offset-0 col-md-2 col-sm-2" style="margin-top: 1%; background:black; height:325px; margin-top: 0px;"> 
+
+			    <nav class="navbar navbar-inverse" style="background-color:transparent; border-color:transparent; display: flex; justify-content: center;">
+
+			        <ul class="nav navbar-nav">
+			            
+			                <li><a class="navbar-brand"><span class="material-icons">home</a></li>
+			                <li><a><span class="material-icons">mail</span></a></li>
+			                <li><a><span class="material-icons">assignment_ind</span></a></li>
+			                <li><a><span class="material-icons">person</span></a></li>
+			                <li><a><span class="material-icons">person_add</span></a></li>
+			       
+			            
+			        </ul>
+			    </nav>
+			    
+			    <img style="height: 250px; margin-bottom: 12%;" src="../Style/IMG/Logo/iamlogo2.png">
+
+		</div>
+
+		<div class="container-fluid col-lg-8 col-lg-offset-0 col-md-8 col-sm-8" style="background-color: transparent; border-color:#d01d3a; height:325px; color: white;">
+
+			<div class=" container-fluid panel panel-inverse col-lg-4 col-md-4 col-sm-4" style="margin-top: 1%; height:300px; background: transparent;"> 
+
+	        <img src="../Style/IMG/Logo/dreamstime_l_28065567.jpg" class="img-rectangle" alt="Cinque Terre" width="100%" height="300">
+
+	   		</div>
+
+		   	<div class=" col-lg-8 col-md-8 col-sm-8" style="margin-top: 1%;text-align: left;">
+
+			    <ul class="pull-right nav">
+			        <li><button class=" btn btn-xs btn-primary" style="background-color:black; border-color: black; width: 150px; margin-bottom: 5px;">Send I.am card</button></li>
+			        <li><button class=" btn btn-xs btn-primary" style="background-color:grey; border-color: grey; width: 150px;">My Resume Portfolio</button></li>
+			    </ul>
+		    
+		    <div class="pull-left">
+				<h1><?php echo $_SESSION['Job_Title']; ?></h1>
+				<h4><?php echo $_SESSION['Comapny_N']; ?></h4>
+				<h4><?php echo $_SESSION['Comapny_E']; ?></h4>
+
+				<p><?php echo $_SESSION['Profile_Descrip']; ?></p>
+			</div>
+
+			</div>
 
 
-<style>
-@import "bootstrap/bootstrap-flex";
 
-@media (min-width:34em) {
-    .card-deck > .card
-    {
-        width: 29%;
-        flex-wrap: wrap;
-        flex: initial; 
-    }
-}
+		</div>
 
+	</div>
 
-/* control image height */
-.card-img-top-250 {
-    max-height: 250px;
-    overflow:hidden;
-}
+	<!-- middle elements -->
+	<div class=" container col-lg-12 col-md-12 col-sm-12" style="height:160px; background-color: black;"> 
 
-/* smoother transitions */
-.carousel-inner>.carousel-item.next.left,
-.carousel-inner>.carousel-item.prev.right
-{
-    transition: all 1s ease;
-}
+			<div class="card-group col-lg-6" style="display: flex;">
+			  <div class="card">
+			    <img class="card-img-top" src="..." alt="Card image cap">
+			    <div class="card-block">
+			      <h4 class="card-title">Card title</h4>
+			      <p class="card-text"></p>
+			      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+			    </div>
+			  </div>
+			  <div class="card">
+			    <img class="card-img-top" src="..." alt="Card image cap">
+			    <div class="card-block">
+			      <h4 class="card-title">Card title</h4>
+			      <p class="card-text"></p>
+			      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+			    </div>
+			  </div>
+			  <div class="card">
+			    <img class="card-img-top" src="..." alt="Card image cap">
+			    <div class="card-block">
+			      <h4 class="card-title">Card title</h4>
+			      <p class="card-text"></p>
+			      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+			    </div>
+			  </div>
+			  <div class="card">
+			    <img class="card-img-top" src="..." alt="Card image cap">
+			    <div class="card-block">
+			      <h4 class="card-title">Card title</h4>
+			      <p class="card-text"></p>
+			      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+			    </div>
+			  </div>
+			  <div class="card">
+			    <img class="card-img-top" src="..." alt="Card image cap">
+			    <div class="card-block">
+			      <h4 class="card-title">Card title</h4>
+			      <p class="card-text"></p>
+			      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+			    </div>
+			  </div>
 
-</style>
+			</div>
 
-<script type="text/javascript">
-	
-(function($) {
-    "use strict";
+			<div class="card-group col-lg-6" style="display: flex; justify-content: center;">
+			  <div class="card">
+			    <img class="card-img-top" src="..." alt="Card image cap">
+			    <div class="card-block">
+			      <h4 class="card-title">Card title</h4>
+			      <p class="card-text"></p>
+			      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+			    </div>
+			  </div>
+			  <div class="card">
+			    <img class="card-img-top" src="..." alt="Card image cap">
+			    <div class="card-block">
+			      <h4 class="card-title">Card title</h4>
+			      <p class="card-text"></p>
+			      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+			    </div>
+			  </div>
+			  <div class="card">
+			    <img class="card-img-top" src="..." alt="Card image cap">
+			    <div class="card-block">
+			      <h4 class="card-title">Card title</h4>
+			      <p class="card-text"></p>
+			      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+			    </div>
+			  </div>
+			  <div class="card">
+			    <img class="card-img-top" src="..." alt="Card image cap">
+			    <div class="card-block">
+			      <h4 class="card-title">Card title</h4>
+			      <p class="card-text"></p>
+			      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+			    </div>
+			  </div>
+			  <div class="card">
+			    <img class="card-img-top" src="..." alt="Card image cap">
+			    <div class="card-block">
+			      <h4 class="card-title">Card title</h4>
+			      <p class="card-text"></p>
+			      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+			    </div>
+			  </div>
+			</div>
+	</div>
 
-    // manual carousel controls
-    $('.next').click(function(){ $('.carousel').carousel('next');return false; });
-    $('.prev').click(function(){ $('.carousel').carousel('prev');return false; });
-    
-})(jQuery);
+	<div class=" container col-lg-12 col-md-12 col-sm-12" style="height:160px; background-color: black; margin-top: 5px;"> 
 
-</script>
+			<div class="card-group" style="display: flex; justify-content: center;">
+			  <div class="card">
+			    <img class="card-img-top" src="..." alt="Card image cap">
+			    <div class="card-block">
+			      <h4 class="card-title">Card title</h4>
+			      <p class="card-text"></p>
+			      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+			    </div>
+			  </div>
+			  <div class="card">
+			    <img class="card-img-top" src="..." alt="Card image cap">
+			    <div class="card-block">
+			      <h4 class="card-title">Card title</h4>
+			      <p class="card-text"></p>
+			      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+			    </div>
+			  </div>
+			  <div class="card">
+			    <img class="card-img-top" src="..." alt="Card image cap">
+			    <div class="card-block">
+			      <h4 class="card-title">Card title</h4>
+			      <p class="card-text"></p>
+			      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+			    </div>
+			  </div>
+			  <div class="card">
+			    <img class="card-img-top" src="..." alt="Card image cap">
+			    <div class="card-block">
+			      <h4 class="card-title">Card title</h4>
+			      <p class="card-text"></p>
+			      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+			    </div>
+			  </div>
+			  <div class="card">
+			    <img class="card-img-top" src="..." alt="Card image cap">
+			    <div class="card-block">
+			      <h4 class="card-title">Card title</h4>
+			      <p class="card-text"></p>
+			      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+			    </div>
+			  </div>
+			  <div class="card">
+			    <img class="card-img-top" src="..." alt="Card image cap">
+			    <div class="card-block">
+			      <h4 class="card-title">Card title</h4>
+			      <p class="card-text"></p>
+			      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+			    </div>
+			  </div>
+			  <div class="card">
+			    <img class="card-img-top" src="..." alt="Card image cap">
+			    <div class="card-block">
+			      <h4 class="card-title">Card title</h4>
+			      <p class="card-text"></p>
+			      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+			    </div>
+			  </div>
+			  <div class="card">
+			    <img class="card-img-top" src="..." alt="Card image cap">
+			    <div class="card-block">
+			      <h4 class="card-title">Card title</h4>
+			      <p class="card-text"></p>
+			      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+			    </div>
+			  </div>
+			  <div class="card">
+			    <img class="card-img-top" src="..." alt="Card image cap">
+			    <div class="card-block">
+			      <h4 class="card-title">Card title</h4>
+			      <p class="card-text"></p>
+			      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+			    </div>
+			  </div>
+			  <div class="card">
+			    <img class="card-img-top" src="..." alt="Card image cap">
+			    <div class="card-block">
+			      <h4 class="card-title">Card title</h4>
+			      <p class="card-text"></p>
+			      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+			    </div>
+			  </div>
+			</div>
+	</div>
 
-<body id="body" style="width:95%; margin-left: auto; margin-right:auto;"> 
+	<div class=" container col-lg-12 col-md-12 col-sm-12" style="height:160px; background-color: black; margin-top: 5px;"> 
 
-<section class="container p-t-3">
-    <div class="row">
-        <div class="col-lg-12">
-            <h1>Bootstrap 4 Card Deck Slider</h1>
-        </div>
-    </div>
-</section>
-<section class="carousel slide" data-ride="carousel" id="postsCarousel">
-    <div class="container">
-        <div class="row">
-            <div class="col-xs-12 text-md-right lead">
-                <a class="btn btn-secondary-outline prev" href="" title="go back"><i class="fa fa-lg fa-chevron-left"></i></a>
-                <a class="btn btn-secondary-outline next" href="" title="more"><i class="fa fa-lg fa-chevron-right"></i></a>
-            </div>
-        </div>
-    </div>
-    <div class="container p-t-0 m-t-2">
-        <div class="row m-t-0">
-            <div class="col-md-12">
-                <div class="carousel-inner">
-                <div class="card-deck carousel-item active">
-                    <div class="card">
-                        <div class="card-img-top card-img-top-250">
-                            <img class="img-fluid" src="http://i.imgur.com/EW5FgJM.png" alt="Carousel 1">
-                        </div>
-                        <div class="card-block p-t-2">
-                            <h6 class="small text-wide p-b-2">Insight</h6>
-                            <h2>
-                            <a href="">Why Stuff Happens Every Year.</a>
-                        </h2>
-                        </div>
-                    </div>
+		<div class=" container col-lg-12" style="display: flex; height:160px; margin: 10px;">
 
-                    <div class="card">
-                        <div class="card-img-top card-img-top-250">
-                            <img class="img-fluid" src="http://i.imgur.com/Hw7sWGU.png" alt="Carousel 2">
-                        </div>
-                        <div class="card-block p-t-2">
-                            <h6 class="small text-wide p-b-2">Development</h6>
-                            <h2>
-                            <a href="">How to Make Every Line Count.</a>
-                        </h2>
-                        </div>
-                    </div>
+		 	<div class=" col-lg-4 panel panel-inverse" style="margin-right: 10px;"> 
 
-                    <div class="card">
-                        <div class="card-img-top card-img-top-250">
-                            <img class="img-fluid" src="http://i.imgur.com/g27lAMl.png" alt="Carousel 3">
-                        </div>
-                        <div class="card-block p-t-2">
-                            <h6 class="small text-wide p-b-2">Design</h6>
-                            <h2>
-                            <a href="">Responsive is Essential.</a>
-                        </h2>
-                        </div>
-                    </div>
-                </div>
-                <div class="card-deck carousel-item">
-                    <div class="card">
-                        <div class="card-img-top card-img-top-250">
-                            <img class="img-fluid" src="//visualhunt.com/photos/l/1/office-student-work-study.jpg" alt="Carousel 4">
-                        </div>
-                        <div class="card-block p-t-2">
-                            <h6 class="small text-wide p-b-2">Another</h6>
-                            <h2>
-                            <a href="">Tagline or Call-to-action.</a>
-                        </h2>
-                        </div>
-                    </div>
+		 	</div>
 
+		 	<div class=" col-lg-4 panel panel-inverse" style="margin-right: 10px;">
 
-                    <div class="card">
-                        <div class="card-img-top card-img-top-250">
-                            <img class="img-fluid" src="//visualhunt.com/photos/l/1/working-woman-technology-computer.jpg" alt="Carousel 5">
-                        </div>
-                        <div class="card-block p-t-2">
-                            <h6 class="small text-wide p-b-2"><span class="pull-xs-right">12.04</span> Category 1</h6>
-                            <h2>
-                            <a href="">This is a Blog Title.</a>
-                        </h2>
-                        </div>
-                    </div>
+		 	</div>
 
-                    <div class="card">
-                        <div class="card-img-top card-img-top-250">
-                            <img class="img-fluid" src="//visualhunt.com/photos/l/1/people-office-team-collaboration.jpg" alt="Carousel 6">
-                        </div>
-                        <div class="card-block p-t-2">
-                            <h6 class="small text-wide p-b-2">Category 3</h6>
-                            <h2>
-                            <a href="">Catchy Title of a Blog Post.</a>
-                        </h2>
-                        </div>
-                    </div>
-                </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</section>
+		 	<div class=" col-lg-4 panel panel-inverse">
 
+		 	</div>
+		</div>
 
-<?php 
-//include "../Functions/footer.php";
-?>
+	</div>
+
+<div class=" container col-lg-12 col-md-12 col-sm-12" style="height:160px; background-color: black; margin-top: 5px;"> 
+
+	<div class=" container col-lg-12" style="display: flex; height:160px; margin: 10px;">
+
+		 	<div class=" col-lg-4 panel panel-inverse" style="margin-right: 10px;"> 
+
+		 	</div>
+
+		 	<div class=" col-lg-4 panel panel-inverse" style="margin-right: 10px;">
+
+		 	</div>
+
+		 	<div class=" col-lg-4 panel panel-inverse">
+
+		 	</div>
+		</div>
+</div>
+
+<div class=" container col-lg-12 col-md-12 col-sm-12" style="height:160px; background-color: black; margin-top: 5px;"> 
+
+			<div class="col-lg-2">
+
+			</div>
+
+			<div class="card-group col-lg-10" style="display: flex; justify-content:center;">
+			  <div class="card">
+			    <img class="card-img-top" src="..." alt="Card image cap">
+			    <div class="card-block">
+			      <h4 class="card-title">Card title</h4>
+			      <p class="card-text"></p>
+			      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+			    </div>
+			  </div>
+			  <div class="card">
+			    <img class="card-img-top" src="..." alt="Card image cap">
+			    <div class="card-block">
+			      <h4 class="card-title">Card title</h4>
+			      <p class="card-text"></p>
+			      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+			    </div>
+			  </div>
+			  <div class="card">
+			    <img class="card-img-top" src="..." alt="Card image cap">
+			    <div class="card-block">
+			      <h4 class="card-title">Card title</h4>
+			      <p class="card-text"></p>
+			      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+			    </div>
+			  </div>
+			  <div class="card">
+			    <img class="card-img-top" src="..." alt="Card image cap">
+			    <div class="card-block">
+			      <h4 class="card-title">Card title</h4>
+			      <p class="card-text"></p>
+			      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+			    </div>
+			  </div>
+
+</div>
